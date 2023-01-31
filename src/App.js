4d8039afd99c9489b225852edc396a99586b0ef9
Rohit kumar <rohitkumarsinghmuz54@gmail.com>
2023-02-01 00:47:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
+  const getdata = () => {
+    try {
+      fetch("https://fakestoreapi.com/products")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "40%", margin: "10px auto" }}>
+      <h2>App Page</h2>
+      <button onClick={getdata}>Getdata</button>
     </div>
   );
-}
+};
 
 export default App;
